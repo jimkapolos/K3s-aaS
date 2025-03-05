b30673e925213984ce9a5d753ca148ee9c66586b
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-05 13:55:39 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "opentofu/kubernetes"
+      version = "2.36.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "vm_instance" "example" {
+  count = var.vm_count
+  # Ρυθμίσεις για το VM (π.χ. image, τύπος, περιοχή, κλπ.)
+}
+
+variable "vm_count" {
+  description = "Number of VMs to deploy"
+  type        = number
+  default     = 2
+}
