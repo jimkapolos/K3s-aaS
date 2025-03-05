7c4e1c4a1801d6fa2d0ab5febf5534bd192281db
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-05 13:42:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "opentofu/kubernetes"
+      version = "2.36.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  # Configuration options
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+}
+
 resource "vm_instance" "example" {
   count = var.vm_count
   # Ρυθμίσεις για το VM (π.χ. image, τύπος, περιοχή, κλπ.)
