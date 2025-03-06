ccb7d64145ecc5bcd739e209e0fd5a71ebd3a31e
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-06 12:30:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,198 @@
-
 terraform {
   required_providers {
-    kubernetes = {
-      source = "opentofu/kubernetes"
-      version = "2.36.0"
+    kubevirt = {
+      source  = "kubevirt/kubevirt"
+      version = "0.0.1"
     }
   }
 }
 
+provider "kubevirt" {
+  config_context = "kubernetes-admin@kubernetes"
+}
 
 
-variable "vm_count" {
-  description = "Number of VMs to deploy"
-  type        = number
-  default     = 2
+resource "kubevirt_virtual_machine" "k3s_master" {
+  metadata {
+    name      = "k3s-master"
+    namespace = "default"
+    annotations = {
+      "kubevirt.io/domain" = "k3s-master"
+    }
+  }
+
+  spec {
+    run_strategy = "Always"
+
+    data_volume_templates {
+      metadata {
+        name      = "ubuntu-disk1"
+        namespace = "default"
+      }
+      spec {
+        pvc {
+          access_modes = ["ReadWriteOnce"]
+          resources {
+            requests = {
+              storage = "10Gi"
+            }
+          }
+        }
+        source {
+          http {
+            url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+          }
+        }
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          "kubevirt.io/domain" = "k3s-master"
+        }
+      }
+      spec {
+        domain {
+          devices {
+            disk {
+              name = "rootdisk"
+              disk_device {
+                disk {
+                  bus = "virtio"
+                }
+              }
+            }
+
+            disk {
+              name = "cloudinitdisk"
+              disk_device {
+                disk {
+                  bus = "virtio"
+                }
+              }
+            }
+
+            interface {
+              name                     = "default"
+              interface_binding_method = "InterfaceMasquerade"
+            }
+          }
+          resources {
+            requests = {
+              cpu    = "2"
+              memory = "2Gi"
+            }
+          }
+        }
+
+        network {
+          name = "default"
+          network_source {
+            pod {}
+          }
+        }
+
+        volume {
+          name = "rootdisk"
+          volume_source {
+            data_volume {
+              name = "ubuntu-disk1"
+            }
+          }
+        }
+
+        volume {
+          name = "cloudinitdisk"
+          volume_source {
+            cloud_init_config_drive {
+              user_data = <<EOF
+#cloud-config
+ssh_pwauth: true
+users:
+  - name: apel
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    groups: users, admin
+    shell: /bin/bash
+    lock_passwd: false
+chpasswd:
+  list: |
+    apel:apel1234
+  expire: false
+
+write_files:
+  - path: /usr/local/bin/k3s-setup.sh
+    permissions: "0755"
+    content: |
+      #!/bin/bash
+      echo "apel ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+      sudo apt-get update
+      sudo apt-get install -y bash-completion sshpass uidmap ufw
+      echo "source <(kubectl completion bash)" >> ~/.bashrc
+      echo "export KUBE_EDITOR=\"/usr/bin/nano\"" >> ~/.bashrc
+      wget https://github.com/containerd/nerdctl/releases/download/v1.7.6/nerdctl-full-1.7.6-linux-amd64.tar.gz
+      sudo tar Cxzvvf /usr/local nerdctl-full-1.7.6-linux-amd64.tar.gz
+      cd /usr/local/bin && containerd-rootless-setuptool.sh install
+      sudo ufw disable
+      curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--cluster-cidr=20.10.0.0/16" sh
+      sudo apt update
+      sudo apt install -y sshpass
+      export VM_IP=$(sshpass -p "apel1234" ssh -o StrictHostKeyChecking=no apel@192.168.188.201 "IP_ADDRESS=\$(kubectl --kubeconfig=/home/apel/.kube/config get vmi k3s-master -o jsonpath='{.status.interfaces[0].ipAddress}'); export K3S_MASTER_IP=\$IP_ADDRESS; echo \$K3S_MASTER_IP")
+      export K3S_TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
+      sshpass -p "apel1234" ssh -o StrictHostKeyChecking=no apel@150.140.187.77 "curl -sfL https://get.k3s.io | K3S_URL=https://$VM_IP:6443 K3S_TOKEN=$K3S_TOKEN sh -"
+
+  - path: /etc/systemd/system/k3s-setup.service
+    permissions: "0644"
+    content: |
+      [Unit]
+      Description=Setup K3s Master Node
+      After=network.target
+      Wants=network-online.target
+
+      [Service]
+      Type=oneshot
+      ExecStart=/usr/local/bin/k3s-setup.sh
+      RemainAfterExit=true
+
+      [Install]
+      WantedBy=multi-user.target
+
+runcmd:
+  - systemctl daemon-reload
+  - systemctl enable k3s-setup.service
+  - systemctl start k3s-setup.service
+EOF
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "kubernetes-admin@kubernetes"
+}
+
+resource "kubernetes_service" "vmi_nodeport_service" {
+  metadata {
+    name      = "vmi-nodeport"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      "kubevirt.io/domain" = "k3s-master"
+    }
+
+    port {
+      protocol    = "TCP"
+      port        = 22
+      target_port = 22
+      node_port   = 30022
+    }
+
+    type = "NodePort"
+  }
 }
