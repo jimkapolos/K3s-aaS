0973340faaf2094d8a2017f37c083c3db0f4cdab
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-07 13:11:02 +0200

    Update main.tf

--- master/main.tf
+++ master/main.tf
@@ -1,4 +1,3 @@
-# master/main.tf
 terraform {
   required_providers {
     kubevirt = {
@@ -20,10 +19,142 @@ resource "kubevirt_virtual_machine" "github-action" {
       "kubevirt.io/domain" = "github-action"
     }
   }
-  # ... rest of your existing configuration
+  
+  # Make sure to include the spec block as in your original configuration
+  spec {
+    run_strategy = "Always"
+    data_volume_templates {
+      metadata {
+        name      = "ubuntu-disk4"
+        namespace = "default"
+      }
+      spec {
+        pvc {
+          access_modes = ["ReadWriteOnce"]
+          resources {
+            requests = {
+              storage = "10Gi"
+            }
+          }
+        }
+        source {
+          http {
+            url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+          }
+        }
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          "kubevirt.io/domain" = "github-action"
+        }
+      }
+      spec {
+        domain {
+          devices {
+            disk {
+              name = "rootdisk"
+              disk_device {
+                disk {
+                  bus = "virtio"
+                }
+              }
+            }
+            disk {
+              name = "cloudinitdisk"
+              disk_device {
+                disk {
+                  bus = "virtio"
+                }
+              }
+            }
+            interface {
+              name                     = "default"
+              interface_binding_method = "InterfaceMasquerade"
+            }
+          }
+          resources {
+            requests = {
+              cpu    = "2"
+              memory = "2Gi"
+            }
+          }
+        }
+        network {
+          name = "default"
+          network_source {
+            pod {}
+          }
+        }
+        volume {
+          name = "rootdisk"
+          volume_source {
+            data_volume {
+              name = "ubuntu-disk4"
+            }
+          }
+        }
+        volume {
+          name = "cloudinitdisk"
+          volume_source {
+            cloud_init_config_drive {
+              user_data = <<EOF
+#cloud-config
+ssh_pwauth: true
+users:
+  - name: apel
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    groups: users, admin
+    shell: /bin/bash
+    lock_passwd: false
+chpasswd:
+  list: |
+    apel:apel1234
+  expire: false
+write_files:
+  - path: /usr/local/bin/k3s-setup.sh
+    permissions: "0755"
+    content: |
+      #!/bin/bash
+      echo "apel ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+      sudo apt-get update
+      sudo apt-get install -y bash-completion sshpass uidmap ufw
+      echo "source <(kubectl completion bash)" >> ~/.bashrc
+      echo "export KUBE_EDITOR=\"/usr/bin/nano\"" >> ~/.bashrc
+      wget https://github.com/containerd/nerdctl/releases/download/v1.7.6/nerdctl-full-1.7.6-linux-amd64.tar.gz
+      sudo tar Cxzvvf /usr/local nerdctl-full-1.7.6-linux-amd64.tar.gz
+      cd /usr/local/bin && containerd-rootless-setuptool.sh install
+      sudo ufw disable
+      curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--cluster-cidr=20.10.0.0/16" sh
+      
+  - path: /etc/systemd/system/k3s-setup.service
+    permissions: "0644"
+    content: |
+      [Unit]
+      Description=Setup K3s Master Node
+      After=network.target
+      Wants=network-online.target
+      [Service]
+      Type=oneshot
+      ExecStart=/usr/local/bin/k3s-setup.sh
+      RemainAfterExit=true
+      [Install]
+      WantedBy=multi-user.target
+runcmd:
+  - systemctl daemon-reload
+  - systemctl enable k3s-setup.service
+  - systemctl start k3s-setup.service
+EOF
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
-# Add a null_resource to wait for the VM to be ready and get the IP and token
+# Add the null_resource for getting the master information
 resource "null_resource" "get_master_info" {
   depends_on = [kubevirt_virtual_machine.github-action]
 
