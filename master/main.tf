9e2b07c440a2626bd8626ed8b2f2c1843f2f07fd
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-13 14:58:45 +0200

    Update main.tf

--- master/main.tf
+++ master/main.tf
@@ -204,15 +204,15 @@ resource "kubernetes_service" "github_nodeport_service" {
 }
 
 output "master_ip_command" {
-  value = "Για να πάρεις την IP του master: kubectl get vmi -n ${var.namespace} github-action-master-${var.namespace} -o jsonpath='{.status.interfaces[0].ip}'"
+  value = "Για να πάρεις την IP του master: kubectl get vmi -n ${var.namespace} github-action-master-${var.namespace} -o jsonpath='{.status.interfaces[0].ipAddress}'"
 }
 
-output "kubeconfig_command" {
-  value = <<EOT
-Για να πάρεις το kubeconfig και να το αποθηκεύσεις τοπικά:
-kubectl get secret -n kube-system k3s -o jsonpath='{.data.config}' | base64 --decode > kubeconfig
-EOT
-}
+#output "kubeconfig_command" {
+ # value = <<EOT
+#Για να πάρεις το kubeconfig και να το αποθηκεύσεις τοπικά:
+#kubectl get secret -n kube-system k3s -o jsonpath='{.data.config}' | base64 --decode > kubeconfig
+#EOT
+#}
 
 output "join_token_command" {
   value = <<EOT
