3c7cb62eef3c3a9927e12ba6ff40be166c4edf73
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-13 15:31:16 +0200

    Update main.tf

--- master/main.tf
+++ master/main.tf
@@ -24,12 +24,9 @@ resource "kubernetes_namespace" "namespace" {
 }
 
 resource "kubevirt_virtual_machine" "github-action-master" {
-  for_each = toset([var.namespace])
   metadata {
-    name      = "github-action-master-${each.key}"
-    namespace = each.key
-  }
-
+    name      = "github-action-master-${var.namespace}"
+    namespace = var.namespace
     annotations = {
       "kubevirt.io/domain" = "github-action-master-${var.namespace}"
     }
@@ -37,90 +34,89 @@ resource "kubevirt_virtual_machine" "github-action-master" {
 
   spec {
     run_strategy = "Always"
+  }
 
-    data_volume_templates {
-      metadata {
-        name      = "ubuntu-disk-master-${var.namespace}"
-        namespace = var.namespace
-      }
-      spec {
-        pvc {
-          access_modes = ["ReadWriteOnce"]
-          resources {
-            requests = {
-              storage = "10Gi"
-            }
+  data_volume_templates {
+    metadata {
+      name      = "ubuntu-disk-master-${var.namespace}"
+      namespace = var.namespace
+    }
+    spec {
+      pvc {
+        access_modes = ["ReadWriteOnce"]
+        resources {
+          requests = {
+            storage = "10Gi"
           }
         }
-        source {
-          http {
-            url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
-          }
+      }
+      source {
+        http {
+          url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
         }
       }
     }
+  }
 
-    template {
-      metadata {
-        labels = {
-          "kubevirt.io/domain" = "github-action-master-${var.namespace}"
+  template {
+    metadata {
+      labels = {
+        "kubevirt.io/domain" = "github-action-master-${var.namespace}"
+      }
+    }
+    spec {
+      domain {
+        devices {
+          disk {
+            name = "rootdisk"
+            disk_device {
+              disk {
+                bus = "virtio"
+              }
+            }
+          }
+          disk {
+            name = "cloudinitdisk"
+            disk_device {
+              disk {
+                bus = "virtio"
+              }
+            }
+          }
+          interface {
+            name                     = "default"
+            interface_binding_method = "InterfaceMasquerade"
+          }
+        }
+        resources {
+          requests = {
+            cpu    = "2"
+            memory = "2Gi"
+          }
         }
       }
-      spec {
-        domain {
-          devices {
-            disk {
-              name = "rootdisk"
-              disk_device {
-                disk {
-                  bus = "virtio"
-                }
-              }
-            }
 
-            disk {
-              name = "cloudinitdisk"
-              disk_device {
-                disk {
-                  bus = "virtio"
-                }
-              }
-            }
+      networks {
+        name = "default"
+        network_source {
+          pod {}
+        }
+      }
 
-            interface {
-              name                     = "default"
-              interface_binding_method = "InterfaceMasquerade"
-            }
-          }
-          resources {
-            requests = {
-              cpu    = "2"
-              memory = "2Gi"
-            }
+      volumes {
+        name = "rootdisk"
+        volume_source {
+          data_volume {
+            name = "ubuntu-disk-master-${var.namespace}"
           }
         }
+      }
 
-        network {
-          name = "default"
-          network_source {
-            pod {}
-          }
-        }
-
-        volume {
-          name = "rootdisk"
-          volume_source {
-            data_volume {
-              name = "ubuntu-disk-master-${var.namespace}"
-            }
-          }
-        }
-
-        volume {
-          name = "cloudinitdisk"
-          volume_source {
-            cloud_init_config_drive {
-              user_data = <<EOF
+      volumes {
+        name = "cloudinitdisk"
+        volume_source {
+          cloud_init_config_drive {
+            user_data = <<EOF
 #cloud-config
 ssh_pwauth: true
 users:
@@ -133,45 +129,7 @@ chpasswd:
   list: |
     apel:apel1234
   expire: false
-
-write_files:
-  - path: /usr/local/bin/k3s-setup.sh
-    permissions: "0755"
-    content: |
-      #!/bin/bash
-      echo "apel ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-      sudo apt-get update
-      sudo apt-get install -y bash-completion sshpass uidmap ufw
-      echo "source <(kubectl completion bash)" >> ~/.bashrc
-      echo "export KUBE_EDITOR=\"/usr/bin/nano\"" >> ~/.bashrc
-      wget https://github.com/containerd/nerdctl/releases/download/v1.7.6/nerdctl-full-1.7.6-linux-amd64.tar.gz
-      sudo tar Cxzvvf /usr/local nerdctl-full-1.7.6-linux-amd64.tar.gz
-      cd /usr/local/bin && containerd-rootless-setuptool.sh install
-      sudo ufw disable
-      curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--cluster-cidr=20.10.0.0/16" sh
-
-  - path: /etc/systemd/system/k3s-setup.service
-    permissions: "0644"
-    content: |
-      [Unit]
-      Description=Setup K3s Master Node
-      After=network.target
-      Wants=network-online.target
-
-      [Service]
-      Type=oneshot
-      ExecStart=/usr/local/bin/k3s-setup.sh
-      RemainAfterExit=true
-
-      [Install]
-      WantedBy=multi-user.target
-
-runcmd:
-  - systemctl daemon-reload
-  - systemctl enable k3s-setup.service
-  - systemctl start k3s-setup.service
 EOF
-            }
           }
         }
       }
@@ -199,7 +157,6 @@ resource "kubernetes_service" "github_nodeport_service" {
       protocol    = "TCP"
       port        = 22
       target_port = 22
-      node_port   = 30021
     }
 
     type = "NodePort"
@@ -207,9 +164,7 @@ resource "kubernetes_service" "github_nodeport_service" {
 }
 
 data "external" "k3s_master_ip" {
-  depends_on = [kubevirt_virtual_machine.github-action-master[var.namespace]]
-
-
+  depends_on = [kubevirt_virtual_machine.github-action-master]
 
   program = ["bash", "-c", <<EOT
 while true; do
@@ -228,4 +183,3 @@ EOT
 output "k3s_master_ip" {
   value = data.external.k3s_master_ip.result["output"]
 }
-
