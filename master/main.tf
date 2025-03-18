1a26cc71341d1e8f2bf54759487e8135734b12ef
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-18 15:15:56 +0200

    Update main.tf

--- master/main.tf
+++ master/main.tf
@@ -170,6 +170,11 @@ write_files:
       [Install]
       WantedBy=multi-user.target
 
+  - path: /home/apel/.ssh/authorized_keys
+    permissions: "0600"
+    owner: "apel"
+    content: |
+        ${data.kubernetes_secret.vm-master-key.data["key1"]}
 
 runcmd:
   - mkdir -p /home/apel/.ssh
