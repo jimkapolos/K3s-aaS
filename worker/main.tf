bf4a05342d22d971186dce8721ab1d493d9272f7
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-18 12:27:05 +0200

    Update main.tf

--- worker/main.tf
+++ worker/main.tf
@@ -52,7 +52,7 @@ resource "kubernetes_namespace" "namespace" {
 resource "kubevirt_virtual_machine" "github-action-agent" {
   metadata {
     name      = "github-action-agent-${var.namespace}"
-    namespace = var.namespace
+    namespace = var.namespace_master
     annotations = {
       "kubevirt.io/domain" = "github-action-agent-${var.namespace}"
     }
