f6799681a9901e4ecb745952815c988d083b5e1b
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-19 16:09:25 +0200

    Update main.tf

--- master/main.tf
+++ master/main.tf
@@ -160,7 +160,7 @@ write_files:
       sudo apt-get update
       mkdir -p ~/.ssh
       chmod 700 ~/.ssh
-      echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRuVXmZ7gXJXEMLL5flwWzEh+oScgRCMEe34Jl+p7IE dkap@apel-server1" >> ~/.ssh/authorized_keys
+      echo ${data.kubernetes_secret.existing_secret.data["key1"]} >> ~/.ssh/authorized_keys
       chmod 600 ~/.ssh/authorized_keys
       sudo systemctl restart ssh
       sudo apt-get install -y bash-completion sshpass uidmap ufw
