9b94d5e317a56e9a6c2f06d93d1c54a7d362e039
dimitriskap <125304112+jimkapolos@users.noreply.github.com>
2025-03-21 10:29:18 +0200

    Update main.tf

--- master/main.tf
+++ master/main.tf
@@ -163,6 +163,12 @@ write_files:
       cd /usr/local/bin && containerd-rootless-setuptool.sh install
       sudo ufw disable
       curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--cluster-cidr=20.10.0.0/16" sh
+      curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+      sudo apt-get install apt-transport-https --yes
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+      sudo apt-get update
+      sudo apt-get install helm
+
 
   - path: /etc/systemd/system/k3s-setup.service
     permissions: "0644"
